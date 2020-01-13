59d33d4268340b9a768d1a9fa3e567378211f7e1
JC7657 <juan.jpjs@gmail.com>
2020-01-13 20:35:57 +0000

    Generator working

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,27 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  console.log(randomExcuse());
 };
+
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function randomExcuse() {
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  let RandomExcuse =
+    who[randomWho] + " " + what[randomWhat] + " " + when[randomWhen];
+  return RandomExcuse;
+}
+
+document.getElementById("rmb").innerHTML = randomExcuse();
